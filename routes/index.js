255cf3685571a7f14f5afb2619a9a144f9453dff
Jihad Hussaini <jihad.akbar.hussaini@gmail.com>
2021-10-12 15:25:19 +0000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ const category = require('./categoriesRoute')
 const user = require('./usersRoute')
 const profile = require('./profilesRoute')
 const transaction = require('./transactionsRoute')
+const safeRoute = require("./safesRoutes");
 
 router.use('/limit', limit)
 router.use('/category', category)
 router.use('/user', user)
 router.use('/profile', profile)
 router.use('/transaction', transaction)
+router.use("/safe", safeRoute);
 
 module.exports = router;
