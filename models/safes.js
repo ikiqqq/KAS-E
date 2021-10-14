067d23910c3901ad987420333b5e71d364078bfe
cutmvasya <meutia.vasya@gmail.com>
2021-10-14 14:58:03 +0700

    fixing transaction

--- models/safes.js
+++ models/safes.js
@@ -1,27 +1,27 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Safes extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      Safes.belongsTo(models.Users, {foreignKey: 'user_id', as: 'user'})
-      Safes.hasMany(models.Transactions, {as: 'transaction'})
-    }
-  };
-  Safes.init({
-    user_id: DataTypes.INTEGER,
-    safeName: DataTypes.STRING,
-    amount: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Safes',
-  });
-  return Safes;
+    class Safes extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            Safes.belongsTo(models.Users, { foreignKey: 'user_id', as: 'user' })
+            Safes.hasMany(models.Transactions, { foreignKey: 'safe_id', as: 'transaction' })
+        }
+    };
+    Safes.init({
+        user_id: DataTypes.INTEGER,
+        safeName: DataTypes.STRING,
+        amount: DataTypes.INTEGER
+    }, {
+        sequelize,
+        modelName: 'Safes',
+    });
+    return Safes;
 };
--- models/transactions.js
+++ models/transactions.js
@@ -1,27 +1,30 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Transactions extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Transactions.init({
-    user_id: DataTypes.INTEGER,
-    limit_id: DataTypes.INTEGER,
-    safe_id: DataTypes.INTEGER,
-    detailExpense: DataTypes.STRING,
-    expense: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Transactions',
-  });
-  return Transactions;
+    class Transactions extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            Transactions.belongsTo(models.Users, { foreignKey: 'user_id', targetKey: 'id' })
+            Transactions.belongsTo(models.Limits, { foreignKey: 'limit_id', targetKey: 'id' })
+            Transactions.belongsTo(models.Safes, { foreignKey: 'safe_id', targetKey: 'id' })
+        }
+    };
+    Transactions.init({
+        user_id: DataTypes.INTEGER,
+        limit_id: DataTypes.INTEGER,
+        safe_id: DataTypes.INTEGER,
+        detailExpense: DataTypes.STRING,
+        expense: DataTypes.INTEGER
+    }, {
+        sequelize,
+        modelName: 'Transactions',
+    });
+    return Transactions;
 };
