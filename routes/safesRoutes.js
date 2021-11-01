568df0a6b03a048697cac4bb8f1ec38c93d9fa92
pandu bhaskara <pandubhaskara40@gmail.com>
2021-11-01 20:02:46 +0700

    add check safe to several routes

--- routes/safesRoutes.js
+++ routes/safesRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const safesController = require("../controllers/safesController");
 const auth = require('../middlewares/authentication')
+const check= require('../middlewares/checkSafes')
 
 router.post("/create", auth, safesController.createSafe);
-router.get("/", auth, safesController.getSafe);
+router.get("/", auth, check, safesController.getSafe);
 router.put("/", auth, safesController.updateSafe);
 router.delete("/:id", auth, safesController.deleteSafe);
 
--- routes/transactionsRoute.js
+++ routes/transactionsRoute.js
@@ -3,9 +3,9 @@ const transactions = require('../controllers/transactionsController')
 const auth = require('../middlewares/authentication')
 const check= require('../middlewares/checkSafes')
 
-router.post('/', auth,check, transactions.postTransaction)
+router.post('/', auth, check, transactions.postTransaction)
 router.post('/addincome', auth, transactions.postAddIncome)
-router.get('/', auth, transactions.getAllTransaction)
+router.get('/', auth, check, transactions.getAllTransaction)
 router.put('/:id', auth, transactions.updateTransaction)
 router.delete('/:id', auth, transactions.deleteTransaction)
 
