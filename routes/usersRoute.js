24c238510a8b7e8a621cf38b85e3cb63ce699497
Jihad Hussaini <86664752+jihadhussaini@users.noreply.github.com>
2021-11-03 14:08:28 +0700

    ok

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -9,7 +9,7 @@ router.post("/login", form.any(), users.login)
 router.post("/register", form.any(), users.register)
 router.get("/verify", users.verifyEmail)
 router.post('/forgot', form.any(), users.forgotPassword)
-router.put('/reset-password/:id/:token', users.resetPassword)
+router.put('/reset-password/:id/:token', form.any(), users.resetPassword)
 router.get("/login/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 router.get("/failed", (req, res)=> res.send("You failed to login, please try again."))
 router.get("/google/callback", passport.authenticate("google", { failureRedirect: "/api/v1/user/failed" }), users.google)
