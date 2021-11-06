1e793c4dbd1cc74dc926239f411326cd72d5700d
Pandu Bhaskara <pandubhaskara40@gmail.com>
2021-11-07 02:02:59 +0700

    fix routes

--- routes/reportsRoute.js
+++ routes/reportsRoute.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const report = require("../controllers/reportsController");
-const auth = require('../middlewares/authentication')
+const auth = require("../middlewares/authentication");
 
 router.get("/daily", auth, report.getDaily);
 router.get("/monthly", auth, report.getMonthly);
